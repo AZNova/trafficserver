7b627b42f98ceeece666da1568bd4ad6caf7cd25
Steven Feltner <sfeltner@godaddy.com>
2018-01-02 18:39:53 -0400

    Added ProxyProtocol v1 and v2 detection and parsing. V2 is not yet complete.

--- proxy/ProtocolProbeSessionAccept.cc
+++ proxy/ProtocolProbeSessionAccept.cc
@@ -47,7 +47,7 @@ proto_is_http2(IOBufferReader *reader)
 }
 
 static bool
-proto_is_proxy_v1(IOBufferReader *reader)
+proto_has_proxy_v1(IOBufferReader *reader, NetVConnection *netvc)
 {
   char buf[PROXY_V1_CONNECTION_PREFACE_LEN];
   char *end;
@@ -61,12 +61,90 @@ proto_is_proxy_v1(IOBufferReader *reader)
     return false;
   }
 
-  ink_assert(nbytes <= (int64_t)PROXY_V1_CONNECTION_PREFACE_LEN);
-  return memcmp(PROXY_V1_CONNECTION_PREFACE, buf, PROXY_V1_CONNECTION_PREFACE_LEN_MIN) == 0;
+  if (memcmp(PROXY_V1_CONNECTION_PREFACE, buf, PROXY_V1_CONNECTION_PREFACE_LEN_MIN) != 0) {
+    return false;
+  }
+
+  int64_t ret;
+  ret = reader->memchr('\r', strlen(buf), 0);
+  ret = reader->memchr('\n', strlen(buf), 0);
+
+  char local_buf[256];   // <- there is a max - LOOK IT UP!
+  reader->read(local_buf, ret+1);
+
+  // Now that we know we have a valid PROXY V1 preface, let's parse the
+  // remainder of the header
+
+  std::string src_addr_port;
+  std::string dst_addr_port;
+
+  char *pch;
+  int cnt = 0;
+  pch = strtok (local_buf," \n\r");
+  while (pch!= NULL) {
+    switch (cnt) {
+      case 0:
+        Debug("proxyprotocol_v1", "proto_has_proxy_v1: token[%d]:[%s] = PREFACE",cnt, pch);
+        break;
+      // After the PROXY, exactly one space followed by the INET protocol family
+      // - TCP4, TCP6 or UNKNOWN
+      case 1:
+        Debug("proxyprotocol_v1", "proto_has_proxy_v1: token[%d]:[%s] = INET Protocol",cnt, pch);
+        break;
+      // Next up is exactly one space and the layer 3 source address
+      // - 255.255.255.255 or ffff:f...f:ffff ffff:f...f:fff
+      case 2:
+        Debug("proxyprotocol_v1", "proto_has_proxy_v1: token[%d]:[%s] = Source Address",cnt, pch);
+        //netvc->set_proxy_protocol_src_addr(addr_port);
+        src_addr_port.assign(pch);
+        break;
+      // Next is exactly one space followed by the layer3 destination address
+      // - 255.255.255.255 or ffff:f...f:ffff ffff:f...f:fff
+      case 3:
+        Debug("proxyprotocol_v1", "proto_has_proxy_v1: token[%d]:[%s] = Destination Address",cnt, pch);
+        dst_addr_port.assign(pch);
+        break;
+      // Next is exactly one space followed by TCP source port represented as a
+      //   decimal number in the range of [0..65535] inclusive.
+      case 4:
+        Debug("proxyprotocol_v1", "proto_has_proxy_v1: token[%d]:[%s] = Source Port",cnt, pch);
+        src_addr_port = src_addr_port + ":" + pch;
+        netvc->set_proxy_protocol_src_addr(ts::string_view(src_addr_port));
+        //netvc->set_proxy_protocol_src_port(1);
+        break;
+      // Next is exactly one space followed by TCP destination port represented as a
+      //   decimal number in the range of [0..65535] inclusive.
+      case 5:
+        Debug("proxyprotocol_v1", "proto_has_proxy_v1: token[%d]:[%s] = Destination Port",cnt, pch);
+        dst_addr_port = dst_addr_port + ":" + pch;
+        netvc->set_proxy_protocol_dst_addr(ts::string_view(dst_addr_port));
+        //netvc->set_proxy_protocol_dst_port(2);
+
+        // THIS RIGHT HERE!  Fill in these fields into the netvc and then pull the 
+        // data back out in the add_forwarded header function!
+        // Oh, and do this in the SSL stuff also!
+
+        break;
+    }
+    // if we have our all of our fields, we are done here
+    //  otherwise increment our field counter and tok another field
+    if (cnt >= 6) {
+      break;
+    } else {
+      ++cnt;
+      pch = strtok (NULL, " \n\r");
+    }
+  }
+
+  // Can I then stash this data away somewhere in the structure so it can be
+  // retrieved before it gets sent down the stack?
+
+  // Make sure we get the required number of fields
+  return (cnt == 6 ? true : false);
 }
 
 static bool
-proto_is_proxy_v2(IOBufferReader *reader)
+proto_has_proxy_v2(IOBufferReader *reader)
 {
   char buf[PROXY_V2_CONNECTION_PREFACE_LEN];
   char *end;
@@ -80,8 +158,85 @@ proto_is_proxy_v2(IOBufferReader *reader)
     return false;
   }
 
-  ink_assert(nbytes <= (int64_t)PROXY_V2_CONNECTION_PREFACE_LEN_MIN);
-  return memcmp(PROXY_V2_CONNECTION_PREFACE, buf, PROXY_V2_CONNECTION_PREFACE_LEN) == 0;
+  if (memcmp(PROXY_V2_CONNECTION_PREFACE, buf, PROXY_V2_CONNECTION_PREFACE_LEN_MIN) != 0) {
+    return false;
+  }
+
+  int64_t ret;
+  ret = reader->memchr('\r', strlen(buf), 0);
+  Debug("proxyprotocol_v2", "proto_has_proxy_v2: reader->memchr(carriage_return) returned:%lld", ret);
+  ret = reader->memchr('\n', strlen(buf), 0);
+  Debug("proxyprotocol_v2", "proto_has_proxy_v2: reader->memchr(new_line) returned:%lld", ret);
+
+  /**
+    Copies and consumes data. Copies len bytes of data from the buffer
+    into the supplied buffer, which must be allocated prior to the call
+    and it must be at large enough for the requested bytes. Once the
+    data is copied, it consumed from the reader.
+
+    @param buf in which to place the data.
+    @param len bytes to copy and consume. If 'len' exceeds the bytes
+      available to the reader, the number of bytes available is used
+      instead.
+
+    @return number of bytes copied and consumed.
+
+  */
+  // inkcoreapi int64_t read(void *buf, int64_t len);
+  char local_buf[256];   // <- there is a max - LOOK IT UP!
+  int64_t reader_len = reader->read(local_buf, ret+1);
+  Debug("proxyprotocol_v2", "proto_has_proxy_v2: read [%s] %zu bytes, reader->read() returned:%lld",
+          local_buf, strlen(local_buf), reader_len);
+
+  // Now that we know we have a valid PROXY V1 preface, let's parse the
+  // remainder of the header
+
+  char * pch;
+  int cnt = 0;
+  pch = strtok (local_buf," \n\r");
+  while (pch!= NULL) {
+    switch (cnt) {
+      case 0:
+        Debug("proxyprotocol_v2", "proto_has_proxy_v2: token[%d]:[%s] = PREFACE",cnt++, pch);
+        break;
+      // After the PROXY, exactly one space followed by the INET protocol family
+      // - TCP4, TCP6 or UNKNOWN
+      case 1:
+        Debug("proxyprotocol_v2", "proto_has_proxy_v2: token[%d]:[%s] = INET Protocol",cnt++, pch);
+        break;
+      // Next up is exactly one space and the layer 3 source address
+      // - 255.255.255.255 or ffff:f...f:ffff ffff:f...f:fff
+      case 2:
+        Debug("proxyprotocol_v2", "proto_has_proxy_v2: token[%d]:[%s] = Source Address",cnt++, pch);
+        break;
+      // Next is exactly one space followed by the layer3 destination address
+      // - 255.255.255.255 or ffff:f...f:ffff ffff:f...f:fff
+      case 3:
+        Debug("proxyprotocol_v2", "proto_has_proxy_v2: token[%d]:[%s] = Destination Address",cnt++, pch);
+        break;
+      // Next is exactly one space followed by TCP source port represented as a
+      //   decimal number in the range of [0..65535] inclusive.
+      case 4:
+        Debug("proxyprotocol_v2", "proto_has_proxy_v2: token[%d]:[%s] = Source Port",cnt++, pch);
+        break;
+      // Next is exactly one space followed by TCP destination port represented as a
+      //   decimal number in the range of [0..65535] inclusive.
+      case 5:
+        Debug("proxyprotocol_v2", "proto_has_proxy_v2: token[%d]:[%s] = Destination Port",cnt++, pch);
+        break;
+    }
+    // if we have our 5 fields, we are done here
+    if (cnt == 6) {
+      break;
+    }
+    pch = strtok (NULL, " \n\r");
+  }
+
+  // Can I then stash this data away somewhere in the structure so it can be
+  // retrieved before it gets sent down the stack?
+
+  // Make sure we get the required number of fields
+  return (cnt == 6 ? true : false);
 }
 
 struct ProtocolProbeTrampoline : public Continuation, public ProtocolProbeSessionAcceptEnums {
@@ -129,13 +284,15 @@ struct ProtocolProbeTrampoline : public Continuation, public ProtocolProbeSessio
       goto done;
     }
 
-    if (proto_is_proxy_v1(reader)) {
-      Debug("http", "ioCompletionEvent: protocol is proxy_v1");
-      key = PROTO_PROXY_V1;
-    } else if (proto_is_proxy_v2(reader)) {
-      Debug("http", "ioCompletionEvent: protocol is proxy_v2");
-      key = PROTO_PROXY_V2;
-    } else if (proto_is_http2(reader)) {
+    if (proto_has_proxy_v1(reader, netvc)) {
+      Debug("http", "ioCompletionEvent: protocol hass proxy_v1");
+      //key = PROTO_PROXY_V1;
+    } else if (proto_has_proxy_v2(reader)) {
+      Debug("http", "ioCompletionEvent: protocol hass proxy_v2");
+      //key = PROTO_PROXY_V2;
+    }
+
+    if (proto_is_http2(reader)) {
       Debug("http", "ioCompletionEvent: protocol is http2");
       key = PROTO_HTTP2;
     } else {
