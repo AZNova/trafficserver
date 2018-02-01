8319e14ab0e8a2a17bf433a3bbe1d08b0099e13d
Steven Feltner <sfeltner@godaddy.com>
2018-02-01 14:07:24 -0400

    Removing unnecessary edits.

--- proxy/http/HttpTransactHeaders.h
+++ proxy/http/HttpTransactHeaders.h
@@ -77,7 +77,6 @@ public:
                                                       ink_time_t now, HTTPHdr *base, HTTPHdr *outgoing);
   static void insert_server_header_in_response(const char *server_tag, int server_tag_size, HTTPHdr *header);
   static void insert_via_header_in_request(HttpTransact::State *s, HTTPHdr *header);
-  static void insert_forwarded_header_in_request(HttpTransact::State *s, HTTPHdr *header);
   static void insert_via_header_in_response(HttpTransact::State *s, HTTPHdr *header);
   static void insert_hsts_header_in_response(HttpTransact::State *s, HTTPHdr *header);
 
